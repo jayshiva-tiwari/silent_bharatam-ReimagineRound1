a3d2ae1b9152e9a423986e5fa79d7cfd88d49bc9
jayshiva-tiwari <139352267+jayshiva-tiwari@users.noreply.github.com>
2024-06-29 22:25:09 +0530

    page-1

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 function loader() {
+    let tl = gsap.timeline();
     
+    tl.from('.load-h1 h1',{
+        y:'100%',
+        duration: 0.5,
+        // delay: 0.2,
+        stagger: 0.3,
+    })
+    tl.from(".load-h1 span",{
+        y:'100%',
+        duration: 0.5,
+        stagger: 0.3,
+    })
+    tl.to('#loader',{
+        display: 'none',
+        duration: 0.8,
+        y:'-100%',
+        // opacity: 0,
+    })
+    tl.from('.page1',{
+        y: '100%',
+        duration: 0.6,
+        ease: Power4,
+    })
 }
+
+
+
+loader()
